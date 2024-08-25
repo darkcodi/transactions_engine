978f57e39defcd2f4d974f2c3f676d2b290c4fcb
darkcodi <trooper982@gmail.com>
2024-08-25 18:40:40 -0500

    Move read_csv and write_csv functions to another mod

--- src/csv_parser.rs
+++ src/csv_parser.rs
@@ -1,8 +1,11 @@
+use std::io;
+use anyhow::Context;
 use serde::{Deserialize, Serialize};
 use thiserror::Error;
 use crate::account::Account;
 use crate::decimal::Decimal4;
-use crate::engine::Operation;
+use crate::engine::{Engine, Operation};
+use crate::storage::EchoDbStorage;
 
 #[derive(Debug, Clone, Deserialize)]
 pub struct CsvOperation {
@@ -76,4 +79,53 @@ pub enum CsvParseError {
 
     #[error("amount cannot be negative")]
     NegativeAmount,
+}
+
+pub async fn read_csv(filepath: &String, engine: &mut Engine<EchoDbStorage>) -> anyhow::Result<u64> {
+    let mut csv_reader = csv::ReaderBuilder::new()
+        .trim(csv::Trim::All)
+        .from_path(filepath)
+        .context("error reading csv file")?;
+
+    let mut counter = 0;
+
+    for deserialize_result in csv_reader.deserialize() {
+        if deserialize_result.is_err() {
+            // eprintln!("csv error: {:?}", deserialize_result.err());
+            continue;
+        }
+        let csv_operation: CsvOperation = deserialize_result.unwrap();
+        let parse_result: Result<Operation, CsvParseError> = csv_operation.try_into();
+        if parse_result.is_err() {
+            // eprintln!("parse error: {:?}", parse_result.err());
+            continue;
+        }
+
+        let operation = parse_result.unwrap();
+        let execution_result = engine.execute_operation(operation).await;
+        if execution_result.is_err() {
+            // eprintln!("execution error: {:?}", execution_result.err());
+            continue;
+        }
+
+        counter += 1;
+    }
+
+    Ok(counter)
+}
+
+pub async fn write_csv(engine: &mut Engine<EchoDbStorage>) -> anyhow::Result<()> {
+    let all_accounts = engine.get_all_accounts().await
+        .context("error getting all accounts")?;
+
+    let mut writer = csv::Writer::from_writer(io::stdout());
+
+    for account in all_accounts {
+        let csv_account: CsvAccount = account.into();
+        writer.serialize(csv_account).context("error writing csv")?;
+    }
+
+    writer.flush().context("error flushing csv")?;
+
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
-use std::io;
-
-use anyhow::Context;
 use clap::{Arg, Command};
-use transactions_engine::engine::{Engine, Operation};
+
+use transactions_engine::csv_parser::{read_csv, write_csv};
+use transactions_engine::engine::Engine;
 use transactions_engine::storage::EchoDbStorage;
-use transactions_engine::csv_parser::{CsvAccount, CsvOperation, CsvParseError};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -24,55 +22,6 @@ async fn main() -> anyhow::Result<()> {
     let mut engine = Engine::new(EchoDbStorage::new());
     read_csv(filepath, &mut engine).await?;
     write_csv(&mut engine).await?;
-
-    Ok(())
-}
-
-async fn read_csv(filepath: &String, engine: &mut Engine<EchoDbStorage>) -> anyhow::Result<u64> {
-    let mut csv_reader = csv::ReaderBuilder::new()
-        .trim(csv::Trim::All)
-        .from_path(filepath)
-        .context("error reading csv file")?;
-
-    let mut counter = 0;
-
-    for deserialize_result in csv_reader.deserialize() {
-        if deserialize_result.is_err() {
-            // eprintln!("csv error: {:?}", deserialize_result.err());
-            continue;
-        }
-        let csv_operation: CsvOperation = deserialize_result.unwrap();
-        let parse_result: Result<Operation, CsvParseError> = csv_operation.try_into();
-        if parse_result.is_err() {
-            // eprintln!("parse error: {:?}", parse_result.err());
-            continue;
-        }
-
-        let operation = parse_result.unwrap();
-        let execution_result = engine.execute_operation(operation).await;
-        if execution_result.is_err() {
-            // eprintln!("execution error: {:?}", execution_result.err());
-            continue;
-        }
-
-        counter += 1;
-    }
-
-    Ok(counter)
-}
-
-async fn write_csv(engine: &mut Engine<EchoDbStorage>) -> anyhow::Result<()> {
-    let all_accounts = engine.get_all_accounts().await
-        .context("error getting all accounts")?;
-
-    let mut writer = csv::Writer::from_writer(io::stdout());
-
-    for account in all_accounts {
-        let csv_account: CsvAccount = account.into();
-        writer.serialize(csv_account).context("error writing csv")?;
-    }
-
-    writer.flush().context("error flushing csv")?;
 
     Ok(())
 }
